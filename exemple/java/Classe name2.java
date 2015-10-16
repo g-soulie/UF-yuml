9ef18efbcf5fa80b3a033b6ab31bb03cd5ece9d6
alcyon <guillaume.soulie@gmail.com>
2015-10-16 22:03:14 +0200

    add UserManual.pdf, add options, fixing bugs

---
+++ exemple/java/Classe name2.java
@@ -0,0 +1,18 @@
+public class Classe name2 {
+	public c Attribut1
+	private d Attribut2
+	
+	public a Method1(){
+	}
+
+	private b Method2()
+	}
+
+	public  c get_Attribut1(){
+		return Attribut1;
+	}
+
+	public  c set_Attribut1(c Attribut1){
+		this.Attribut1=Attribut1;
+	}
+}
